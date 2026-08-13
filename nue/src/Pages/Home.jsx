e59fafe051447ca47e3f31e1c2de83fd5db08d4f
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-08-13 22:01:46 +0200

    halfway done with the ui all thats left is the actuall math of it lol

--- nue/src/Pages/Home.jsx
+++ nue/src/Pages/Home.jsx
@@ -16,11 +16,23 @@ const Home = () => {
         </h2>
       </div>
       <div className="mt-10">
-        <h1 className="text-[#94a3b8]">
-            Ip Address
-        </h1>
-        <input type="text" className="bg-[#334155] p-1 rounded-[5px] mt-5 w-full border-[#94a3b8] border-[2px] text-white"/>
+        <h1 className="text-[#94a3b8]">Ip Address</h1>
+        <input
+          type="text"
+          className="bg-[#334155] p-1 rounded-[5px] mt-5 w-full border-[#94a3b8] border-[2px] text-white"
+        />
       </div>
+      <div className="mt-10">
+        <h1 className="text-[#94a3b8]">Prefix Lenght (CIDR)</h1>
+       <div className="flex items-center gap-2">
+        <p className="text-[#94a3b8] items-center justify-center pt-3">\</p>
+         <input
+          type="text"
+          className="bg-[#334155] p-1 rounded-[5px] mt-5 w-[100px] border-[#94a3b8] border-[2px] text-white"
+        />
+       </div>
+      </div>
+      <button className="w-full bg-[#0ea5e9] mt-10 h-[50px] rounded-[10px] text-white text-[1.5rem] font-extrabold">Caculate</button>
     </div>
   );
 };
