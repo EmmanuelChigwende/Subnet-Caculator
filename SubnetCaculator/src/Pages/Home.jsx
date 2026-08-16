77dcc72954f16716bde823c18b13bf2dd2ce465d
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-08-16 16:59:18 +0200

    making progress i think 😭

--- SubnetCaculator/src/Pages/Home.jsx
+++ SubnetCaculator/src/Pages/Home.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import toast from "react-hot-toast";
+import { useState } from "react";
 
 const Home = () => {
-  //  i know how to do this on paper lol
+  // Handles the valuses put in by the user
+  const [ipAddress, setIpAddress] = useState();
+  const [prefix, setPrefix] = useState();
 
-  const PrefixLenght = 20
+  //  i know how to do this on paper lol
   
-  async function IntrestingOctet(PrefixLenght) {
-    const intrest = PrefixLenght % 8
-    console.log(intrest)
-  }
 
   return (
-    <div   className=" h-[500px] w-[400px] rounded-xl p-2">
+    <div className=" h-[500px] w-[400px] rounded-xl p-2">
       <div>
         <h1 className="text-white text-[2rem] font-extrabold">
           Subnet Caculator
@@ -25,6 +24,9 @@ const Home = () => {
         <h1 className="text-[#94a3b8]">Ip Address</h1>
         <input
           type="text"
+          name="ipaddress"
+          value={ipAddress}
+          onChange={(e) => setIpAddress(e.target.value)}
           className="bg-[#334155] p-1 rounded-[5px] mt-5 w-full border-[#94a3b8] border-[2px] text-white"
         />
       </div>
@@ -34,11 +36,14 @@ const Home = () => {
           <p className="text-[#94a3b8] items-center justify-center pt-3">\</p>
           <input
             type="text"
+            name="prefix"
+            value={prefix}
+            onChange={(e) => setPrefix(e.target.value)}
             className="bg-[#334155] p-1 rounded-[5px] mt-5 w-[100px] border-[#94a3b8] border-[2px] text-white"
           />
         </div>
       </div>
-      <button onClick={IntrestingOctet} className="w-full bg-[#0ea5e9] mt-10 h-[50px] rounded-[10px] text-white text-[1.5rem] font-extrabold">
+      <button className="w-full bg-[#0ea5e9] mt-10 h-[50px] rounded-[10px] text-white text-[1.5rem] font-extrabold">
         Caculate
       </button>
     </div>
